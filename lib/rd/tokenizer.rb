ba0b3eec1b703e6e70dee217e9e52abceac43cec
Casiano Rodriguez <casiano.rodriguez.leon@gmail.com>
2012-04-26 13:04:47 +0100

    introduced module RD in tokenizer

--- lib/rd/tokenizer.rb
+++ lib/rd/tokenizer.rb
@@ -1,41 +1,43 @@
-class Lexer
-   attr_accessor :lex_tokens, :tokens
+module RD
+  class Lexer
+     attr_accessor :lex_tokens, :tokens
 
-   def initialize(&block)
-     @lex_tokens = []
-     @tokens = []
-     instance_eval(&block)
-   end
-
-   def lex(string)
-     until string.empty?
-       there_is_a_match = @lex_tokens.any? do |tok|
-         match = tok.pattern.match(string)
-         if match
-           @tokens << tok.block.call(match.to_s) if tok.block
-           string = match.post_match
-           true
-         else
-           false
-         end
-       end
-       raise "unable to lex '#{string}" unless  there_is_a_match
+     def initialize(&block)
+       @lex_tokens = []
+       @tokens = []
+       instance_eval(&block)
      end
-   end
 
-   private
+     def lex(string)
+       until string.empty?
+         there_is_a_match = @lex_tokens.any? do |tok|
+           match = tok.pattern.match(string)
+           if match
+             @tokens << tok.block.call(match.to_s) if tok.block
+             string = match.post_match
+             true
+           else
+             false
+           end
+         end
+         raise "unable to lex '#{string}" unless  there_is_a_match
+       end
+     end
 
-   LexToken = Struct.new(:pattern, :block)
+     private
 
-    def token(pattern, &block)
-      @lex_tokens << LexToken.new(Regexp.new('\\A(?:' + pattern.source + ')', pattern.options), block)
-    end
+     LexToken = Struct.new(:pattern, :block)
 
+      def token(pattern, &block)
+        @lex_tokens << LexToken.new(Regexp.new('\\A(?:' + pattern.source + ')', pattern.options), block)
+      end
+
+  end
 end
 
 if __FILE__ == $0 then
 
-  lexer = Lexer.new do
+  lexer = RD::Lexer.new do
      token(/\s+/)
      token(/\d+/) {|m| m.to_i }
      token(/./)   {|m| m }
